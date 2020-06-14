4c3a4ad5f77c114691238497ae99e1325f2f42ef
Инна Подистова <gengakutaku@ya.ru>
2020-06-14 21:14:44 +0700

    Добавлен jest и снепшот-тесты компонентов

---
+++ src/components/app/app.test.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import renderer from 'react-test-renderer';
+import App from './app.jsx';
+
+describe(`Render App`, () => {
+  it(`renders App`, () => {
+    const tree = renderer
+      .create(<App errorsCount={3} />)
+      .toJSON();
+
+    expect(tree).toMatchSnapshot();
+  });
+});
