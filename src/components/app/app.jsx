4f847af9beaaa289f6a9fff53f2f463207c83840
Инна Подистова <gengakutaku@ya.ru>
2020-06-21 02:28:12 +0700

    Добавлены компоненты вопросов, реакт-роутер и роутинг

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -1,15 +1,30 @@
 import React from 'react';
-import WelcomeScreen from '../welcome-screen/welcome-screen.jsx';
+import {BrowserRouter, Route, Switch} from 'react-router-dom';
 import PropTypes from 'prop-types';
+import WelcomeScreen from '../welcome-screen/welcome-screen.jsx';
+import QuestionArtist from '../question-artist/question-artist.jsx';
+import QuestionGenre from '../question-genre/question-genre.jsx';
 
 const playButtonClickHandler = () => {};
 
 const App = ({errorsCount}) => {
   return (
-    <WelcomeScreen
-      errorsCount={errorsCount}
-      onPlayButtonClick={playButtonClickHandler}
-    />
+    <BrowserRouter>
+      <Switch>
+        <Route exact path="/">
+          <WelcomeScreen
+            errorsCount={errorsCount}
+            onPlayButtonClick={playButtonClickHandler}
+          />
+        </Route>
+        <Route exact path="/artist">
+          <QuestionArtist />
+        </Route>
+        <Route exact path="/genre">
+          <QuestionGenre />
+        </Route>
+      </Switch>
+    </BrowserRouter>
   );
 };
 
