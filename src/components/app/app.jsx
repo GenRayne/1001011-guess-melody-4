999c4aba6138104935b38312a4ed8dd52614ded2
Инна Подистова <gengakutaku@ya.ru>
2020-06-24 01:18:27 +0700

    Переключение экранов

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -1,21 +1,60 @@
-import React from 'react';
+import React, {useState, useCallback} from 'react';
 import {BrowserRouter, Route, Switch} from 'react-router-dom';
 import {array, number} from 'prop-types';
 import WelcomeScreen from '../welcome-screen/welcome-screen.jsx';
 import QuestionArtistScreen from '../question-artist/question-artist.jsx';
 import QuestionGenreScreen from '../question-genre/question-genre.jsx';
+import {QuestionType} from '../../const.js';
 
-const playButtonClickHandler = () => {};
+const START_STEP = -1;
 
 const App = ({errorsCount, questions}) => {
+  const [step, setStep] = useState(START_STEP);
+
+  const handlePlayButtonClick = useCallback(() => setStep(0), []);
+  const handleAnswer = useCallback(() => setStep((prevStep) => prevStep + 1), []);
+
+  const renderGameScreen = useCallback(
+      () => {
+        const question = questions[step];
+
+        if (step === START_STEP || step >= questions.length) {
+          return (
+            <WelcomeScreen
+              errorsCount={errorsCount}
+              onPlayButtonClick={handlePlayButtonClick}
+            />
+          );
+        }
+
+        if (question) {
+          switch (question.type) {
+            case QuestionType.ARTIST:
+              return (
+                <QuestionArtistScreen
+                  question={questions[1]}
+                  onAnswer={handleAnswer}
+                />
+              );
+            case QuestionType.GENRE:
+              return (
+                <QuestionGenreScreen
+                  question={questions[0]}
+                  onAnswer={handleAnswer}
+                />
+              );
+          }
+        }
+
+        return null;
+      }, [step]
+  );
+
   return (
     <BrowserRouter>
       <Switch>
         <Route exact path="/">
-          <WelcomeScreen
-            errorsCount={errorsCount}
-            onPlayButtonClick={playButtonClickHandler}
-          />
+          {renderGameScreen()}
         </Route>
         <Route exact path="/artist">
           <QuestionArtistScreen question={questions[1]} onAnswer={() => {}} />
