f20b73725bb279db499ef78e914c1af1d768c5f1
Инна Подистова <gengakutaku@ya.ru>
2020-07-05 14:30:35 +0700

    Компонент GameScreen с общей разметкой из компонентов вопросов

---
+++ src/components/game-screen/game-screen.test.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import renderer from 'react-test-renderer';
+import GameScreen from './game-screen.jsx';
+import QuestionArtist from '../question-artist/question-artist.jsx';
+import questions from '../../mocks/test-questions.js';
+import {QuestionType} from '../../const.js';
+
+describe(`render GameScreen`, () => {
+  it(`renders GameScreen`, () => {
+    const handler = () => {};
+
+    const tree = renderer
+      .create(
+          <GameScreen type={QuestionType.ARTIST}>
+            <QuestionArtist question={questions[1]} onAnswer={handler} />
+          </GameScreen>
+      )
+      .toJSON();
+
+    expect(tree).toMatchSnapshot();
+  });
+});
--- src/components/question-artist/question-artist.jsx
+++ src/components/question-artist/question-artist.jsx
@@ -6,40 +6,21 @@ const QuestionArtistScreen = ({question, onAnswer}) => {
   const {song, answers} = question;
 
   return (
-    <section className="game game--artist">
-      <header className="game__header">
-        <a className="game__back" href="#">
-          <span className="visually-hidden">Сыграть ещё раз</span>
-          <img className="game__logo" src="img/melody-logo-ginger.png" alt="Угадай мелодию" />
-        </a>
-
-        <svg xmlns="http://www.w3.org/2000/svg" className="timer" viewBox="0 0 780 780">
-          <circle className="timer__line" cx="390" cy="390" r="370" style={{filter: `url(#blur)`, transform: `rotate(-90deg) scaleY(-1)`, transformOrigin: `center`}} />
-        </svg>
-
-        <div className="game__mistakes">
-          <div className="wrong"></div>
-          <div className="wrong"></div>
-          <div className="wrong"></div>
-        </div>
-      </header>
-
-      <section className="game__screen">
-        <h2 className="game__title">Кто исполняет эту песню?</h2>
-        <div className="game__track">
-          <div className="track">
-            <button className="track__button track__button--play" type="button" />
-            <div className="track__status">
-              <audio src={song.src} />
-            </div>
+    <>
+      <h2 className="game__title">Кто исполняет эту песню?</h2>
+      <div className="game__track">
+        <div className="track">
+          <button className="track__button track__button--play" type="button" />
+          <div className="track__status">
+            <audio src={song.src} />
           </div>
         </div>
+      </div>
 
-        <form className="game__artist">
-          {answers.map((answer) => <AnswerArtist answer={answer} question={question} key={answer.id} onAnswer={onAnswer} />)}
-        </form>
-      </section>
-    </section>
+      <form className="game__artist">
+        {answers.map((answer) => <AnswerArtist answer={answer} question={question} key={answer.id} onAnswer={onAnswer} />)}
+      </form>
+    </>
   );
 };
 
