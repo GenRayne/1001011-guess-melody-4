2b76edc5130708e848abb42d7ee10a80dee11313
Инна Подистова <gengakutaku@ya.ru>
2020-08-26 23:10:16 +0700

    Тесты для функций проверки ответа

--- src/utils.test.js
+++ src/utils.test.js
@@ -1,4 +1,6 @@
 import {capitalizeFirstLetter} from './utils.js';
+import {genreQuestion, artistQuestion} from './mocks/test-questions.js';
+import {isGenreAnswerCorrect, isArtistAnswerCorrect, isAnswerCorrect} from './utils.js';
 
 describe(`capitalize`, () => {
   it(`capitalizes the first letter`, () => {
@@ -6,5 +8,20 @@ describe(`capitalize`, () => {
     const capitalizedWord = capitalizeFirstLetter(word);
 
     expect(capitalizedWord).toEqual(`Pause`);
+  });
+});
+
+describe(`answer correctness`, () => {
+  it(`returns true if genre answer is correct`, () => {
+    expect(isGenreAnswerCorrect(genreQuestion, {1: true})).toEqual(true);
+  });
+
+  it(`returns true if artist answer is correct`, () => {
+    expect(isArtistAnswerCorrect(artistQuestion, {artist: `correct`})).toEqual(true);
+  });
+
+  it(`returns true if answer is correct`, () => {
+    expect(isAnswerCorrect(genreQuestion, {1: true})).toEqual(true);
+    expect(isAnswerCorrect(artistQuestion, {artist: `correct`})).toEqual(true);
   });
 });
